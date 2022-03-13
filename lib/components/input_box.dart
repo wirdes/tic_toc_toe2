c9d7ffd74c3b5b2860c83621e21627ce0077a5e2
wirdes <45904810+wirdes@users.noreply.github.com>
2022-03-13 21:12:28 +0300

    feat:

--- lib/components/input_box.dart
+++ lib/components/input_box.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 
 class InputBox extends StatefulWidget {
-  const InputBox({Key? key, required this.text, required this.colors})
-      : super(key: key);
-
+  const InputBox({
+    Key? key,
+    required this.text,
+    required this.onTapFunc,
+    required this.index,
+  }) : super(key: key);
+  final void Function() onTapFunc;
   final String text;
-  final Color colors;
+  //final Color colors;
+  final int index;
 
   @override
   State<InputBox> createState() => _InputBoxState();
@@ -14,14 +19,17 @@ class InputBox extends StatefulWidget {
 class _InputBoxState extends State<InputBox> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        height: 50,
-        width: 50,
-        child: Center(
-          child: Text(
-            widget.text,
-            style: TextStyle(color: Colors.blue, fontSize: 22),
-          ),
-        ));
+    return InkWell(
+      onTap: widget.onTapFunc,
+      child: SizedBox(
+          height: 50,
+          width: 50,
+          child: Center(
+            child: Text(
+              widget.text,
+              style: const TextStyle(color: Colors.blue, fontSize: 22),
+            ),
+          )),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -27,15 +27,25 @@ class _HomePageState extends State<HomePage> {
   bool isPlayerOne = true;
   List<String> list = ["", "", "", "", "", "", "", "", ""];
   bool isFinish = false;
+  String winner = "";
   static const List<List<int>> isFinishlist = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
   ];
 
   void _finishControl() {
+    if (!list.contains("")) {
+      setState(() {
+        isFinish = true;
+        winner = "No one";
+      });
+    }
     for (var a = 0; a < 5; a++) {
       var control = list[isFinishlist[a][0]];
       var control1 = list[isFinishlist[a][1]];
@@ -44,12 +54,11 @@ class _HomePageState extends State<HomePage> {
       } else if (control == control1 && control2 == control1) {
         setState(() {
           isFinish = true;
+          winner = control;
         });
       }
     }
   }
-
-  // 0 - 1 - 2 "" 3 - 4 - 5 "" 6 - 7 - 8 "" 0 - 4 - 8 "" 2 - 4 - 6
 
   @override
   Widget build(BuildContext context) {
@@ -57,97 +66,85 @@ class _HomePageState extends State<HomePage> {
       body: !isFinish
           ? Center(
               child: SizedBox(
-                height: 700,
-                width: 500,
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        newMethod(0),
-                        Container(
-                          color: Colors.black,
-                          width: 5,
-                          height: 50,
-                        ),
-                        newMethod(1),
-                        Container(
-                          color: Colors.black,
-                          width: 5,
-                          height: 50,
-                        ),
-                        newMethod(2),
-                      ],
+                    rows(0),
+                    const Divider(
+                      isHorizontal: true,
                     ),
-                    Container(
-                      color: Colors.black,
-                      width: 50,
-                      height: 5,
+                    rows(3),
+                    const Divider(
+                      isHorizontal: true,
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        newMethod(3),
-                        Container(
-                          color: Colors.black,
-                          width: 5,
-                          height: 50,
-                        ),
-                        newMethod(4),
-                        Container(
-                          color: Colors.black,
-                          width: 5,
-                          height: 50,
-                        ),
-                        newMethod(5),
-                      ],
-                    ),
-                    Container(
-                      color: Colors.black,
-                      width: 50,
-                      height: 5,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        newMethod(6),
-                        Container(
-                          color: Colors.black,
-                          width: 5,
-                          height: 50,
-                        ),
-                        newMethod(7),
-                        Container(
-                          color: Colors.black,
-                          width: 5,
-                          height: 50,
-                        ),
-                        newMethod(8),
-                      ],
-                    ),
+                    rows(6),
                   ],
                 ),
               ),
             )
-          : Container(),
+          : SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text("Game Finish"),
+                  Text(winner + " Winner"),
+                  InkWell(
+                      onTap: () {
+                        setState(() {
+                          isFinish = false;
+                          isPlayerOne = true;
+                          list = ["", "", "", "", "", "", "", "", ""];
+                        });
+                      },
+                      child: const Text("Play Again ?")),
+                ],
+              ),
+            ),
     );
   }
 
-  InkWell newMethod(int index) {
-    return InkWell(
-      onTap: () {
-        setState(() {
-          if (list[index] != "") return;
-          list[index] = isPlayerOne ? "0" : "X";
-          isPlayerOne = !isPlayerOne;
-        });
-        _finishControl();
-      },
-      child: InputBox(
+  Row rows(int i) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        getInputBox(i),
+        const Divider(),
+        getInputBox(i + 1),
+        const Divider(),
+        getInputBox(i + 2),
+      ],
+    );
+  }
+
+  Widget getInputBox(int index) {
+    return InputBox(
         text: list[index],
-        colors: isPlayerOne ? Colors.red : Colors.blue,
-      ),
+        onTapFunc: () {
+          setState(() {
+            if (list[index] != "") return;
+            list[index] = isPlayerOne ? "0" : "X";
+            isPlayerOne = !isPlayerOne;
+          });
+          _finishControl();
+        },
+        index: index);
+  }
+}
+
+class Divider extends StatelessWidget {
+  const Divider({Key? key, this.isHorizontal = false}) : super(key: key);
+  final bool? isHorizontal;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      width: isHorizontal! ? 150 : 5,
+      height: isHorizontal! ? 5 : 50,
     );
   }
 }
